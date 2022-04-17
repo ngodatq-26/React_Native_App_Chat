13b5beba017eddb12e191ba0930444e72f718a85
ngoquangdatjpnd <66765392+ngoquangdatjpnd@users.noreply.github.com>
2022-04-17 23:52:38 +0700

    ok

--- redux/configureStore.ts
+++ redux/configureStore.ts
@@ -6,7 +6,7 @@ import rootReducer from './reducer';
 import rootSaga from './sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const {store} = createStore(rootReducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(rootSaga);
 
 export default store;
